8b1aa678f8476f42abb242f4c79c25f7ac4407db
azizbek23100307 <azizbekmeliqulov94@gmail.com>
2026-07-24 16:36:15 +0500

    chore: initial project setup with Spring Boot and PostgreSQL

---
+++ src/main/java/xurshid_azizbek/com/example/nasiyabackend/NasiyaBackendApplication.java
@@ -0,0 +1,13 @@
+package xurshid_azizbek.com.example.nasiyabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasiyaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NasiyaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xurshid_azizbek/com/example/nasiyabackend/NasiyaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package xurshid_azizbek.com.example.nasiyabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasiyaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
